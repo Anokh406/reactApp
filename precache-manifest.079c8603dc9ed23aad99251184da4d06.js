a7729e43789e2cb4ee452faa847b7ba9571d8a4d
Anokh406 <95044585+Anokh406@users.noreply.github.com>
2022-05-07 16:07:31 +0530

    Updates

---
+++ precache-manifest.079c8603dc9ed23aad99251184da4d06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b3b7c1e6851fd2f5782",
+    "url": "/reactApp/static/css/main.e5e6a3a3.chunk.css"
+  },
+  {
+    "revision": "7b3b7c1e6851fd2f5782",
+    "url": "/reactApp/static/js/main.7b3b7c1e.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/reactApp/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "1639b18c62923541ce21",
+    "url": "/reactApp/static/js/2.1639b18c.chunk.js"
+  },
+  {
+    "revision": "a6edc5839abe26657593",
+    "url": "/reactApp/static/js/runtime~main.a6edc583.js"
+  },
+  {
+    "revision": "2d82245078cfbd9f9b3302680105b980",
+    "url": "/reactApp/index.html"
+  }
+];
